47fb56be351a551f71ec63f99e67956546aaef25
Sandra Valdivia Gómez <valdivia.gomez.sandra@gmail.com>
2021-05-14 18:46:59 -0500

    limpieza de la rama

--- src/routes/usuario.js
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-impor * as usuario_controller from "../controllers/usuario";
-
-export const usuario_router = Router();
-
-usuario_router.route("/usuarios").post(usuario_controller.crearUsuario);
-
-
